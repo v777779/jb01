57859643f8b35f9f7e92e912520ce4bcdd767eb3
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-03-26 20:29:53 +0300

    project up  ch16 exercise 21

--- src/ch16/ex21/access/Access.java
+++ src/ch16/ex21/access/Access.java
@@ -1,11 +1,49 @@
 package ch16.ex21.access;
 
+import lib.generate.Gen;
+import lib.utils.BerylliumSphere;
+
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Created by V1 on 18-Feb-17.
  */
 public class Access {
     public static void app() {
         System.out.println("\n====ACCESS===");
-        System.out.println("\nExercise Check\n");
+        System.out.println("\nExercise 21 Array Sort Check\n");
+
+        System.out.println("Array Sort:");
+        Random rnd = new Random();
+        BerylliumSphere[] baseArray = Gen.getArray(BerylliumSphere.class,BerylliumSphere.gen(),25);
+        BerylliumSphere[] bArray = new BerylliumSphere[7];
+        BerylliumSphere[] cArray = new BerylliumSphere[7];
+
+        boolean [] checked = new boolean[baseArray.length];
+
+        for (int i = 0; i < bArray.length; i++) {
+            int index = rnd.nextInt(baseArray.length);
+            while (checked[index]) {
+                index = rnd.nextInt(baseArray.length);
+            }
+            checked[index]= true;
+            bArray[i] = baseArray[index];
+            while (checked[index]) {
+                index = rnd.nextInt(baseArray.length);
+            }
+            checked[index]= true;
+            cArray[i] = baseArray[index];
+        }
+
+        System.out.println("unsorted:"+ Arrays.toString(bArray));
+        System.out.println("unsorted:"+ Arrays.toString(cArray));
+        Arrays.sort(bArray);
+        Arrays.sort(cArray, BerylliumSphere.getComp());
+        System.out.println("forward :"+Arrays.toString(bArray));
+        System.out.println("reverse :"+Arrays.toString(cArray));
+
+
+
     }
 }
--- src/lib/utils/BerylliumSphere.java
+++ src/lib/utils/BerylliumSphere.java
@@ -1,14 +1,53 @@
 package lib.utils;
 
+import java.util.Comparator;
+
 /**
  * Created by V1 on 23.03.2017.
  */
-public class BerylliumSphere {
+public class BerylliumSphere implements Comparable<BerylliumSphere> {
     private static long counter;
     private final long id = counter++;
+// Generator
+    public static IGenerator<BerylliumSphere> gen() {
+        return new IGenerator<BerylliumSphere>() {
+            @Override
+            public BerylliumSphere next() {
+                return new BerylliumSphere();
+            }
+        };
+    }
+// Comparator
+    public static Comparator<BerylliumSphere> getComp() {
+        return new Comparator<BerylliumSphere>() {
+            @Override
+            public int compare(BerylliumSphere o1, BerylliumSphere o2) {
+                if (o1.id > o2.id) {
+                    return -1;
+                }
+                if (o1.id < o2.id) {
+                    return 1;
+                }
+                return 0;
+            }
+        };
+    }
+// Comparable
+    @Override
+    public int compareTo(BerylliumSphere o) {
+        if (id < o.id) {
+            return -1;
+        }
+        if (id > o.id) {
+            return 1;
+        }
+        return 0;
+    }
+
 
     @Override
     public String toString() {
         return "Sphere " + id;
     }
+
 }
