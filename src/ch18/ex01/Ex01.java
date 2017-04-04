9aff9a877c19bdcc06cedab14b81df4907950250
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-04-04 15:02:02 +0200

    project up  ch18

---
+++ src/ch18/ex01/Ex01.java
@@ -0,0 +1,15 @@
+package ch18.ex01;
+
+import ch18.ex01.access.Access;
+import ch18.ex01.local.Local;
+
+/**
+ * Created by V1 on 04-Apr-17.
+ */
+public class Ex01 {
+    public static void main(String[] args) {
+        Local.app();
+        Access.app();
+
+    }
+}
