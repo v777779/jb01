70794591808fa34f8e577e4e8ac112efd5f86ecb
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-04-07 13:39:22 +0300

    project up  ch18 exercise 08

--- src/ch18/ex08/Ex08.java
+++ src/ch18/ex08/Ex08.java
@@ -1,7 +1,13 @@
 package ch18.ex08;
 
+import ch18.ex08.access.Access;
+
 /**
  * Created by V1 on 07-Apr-17.
  */
 public class Ex08 {
+    public static void main(String[] args) {
+        Access.app(args);
+
+    }
 }
--- src/ch18/ex08/access/Access.java
+++ src/ch18/ex08/access/Access.java
@@ -1,11 +1,30 @@
 package ch18.ex08.access;
 
+import lib.files.BFileRead;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Created by V1 on 18-Feb-17.
  */
 public class Access {
-    public static void app() {
+    public static void app(String[] args) {
         System.out.println("\n====ACCESS===");
-        System.out.println("\nExercise Check\n");
+        System.out.println("\nExercise 8 FileRead Check\n");
+
+        if (args == null || args.length == 0) {
+            System.out.println("Usage: args[0] <filePath>");
+        }
+        if (args.length == 0) {
+            args = new String[]{"./src/ch18/ex08/access/Access.java"};
+        }
+        List<String> list = new LinkedList<>(BFileRead.readList(args[0]));
+        ListIterator<String> lit = list.listIterator(list.size());
+        while (lit.hasPrevious()) {
+            System.out.println(lit.previous());
+        }
+
     }
 }
