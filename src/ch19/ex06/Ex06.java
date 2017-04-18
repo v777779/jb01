d6ccb21a9a985c6fe72167c33f3d645b16cbfd28
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-04-18 19:48:10 +0300

    project up  ch19 exercise 06

---
+++ src/ch19/ex06/Ex06.java
@@ -0,0 +1,13 @@
+package ch19.ex06;
+
+/**
+ * Copyright (c) 2017 Vadim Voronov
+ * email: vadim.v.voronov@gmail.com
+ * Created: 18-Apr-17.
+ */
+public class Ex06 {
+    public static void main(String[] args) {
+        System.out.println("The nesting enums inside the class makes code clear and compact");
+        System.out.println("That's all according to original solution.");
+    }
+}
