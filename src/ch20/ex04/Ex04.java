92ed3b60d803e7793489cd9a8d5581d0dbc6be88
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-04-25 17:26:02 +0300

    project up added libraries sun.mirror, junit4 v4.12

---
+++ src/ch20/ex04/Ex04.java
@@ -0,0 +1,17 @@
+package ch20.ex04;
+
+import ch20.ex04.codea.CodeA;
+import ch20.ex04.exercise.Exercise;
+
+/**
+ * Copyright (c) 2017 Vadim Voronov
+ * email: vadim.v.voronov@gmail.com
+ * Created: 25-Apr-17.
+ */
+public class Ex04 {
+    public static void main(String[] args) {
+        CodeA.app();
+        Exercise.app();
+
+    }
+}
---
+++ src/ch20/ex04/codea/CodeA.java
@@ -0,0 +1,16 @@
+package ch20.ex04.codea;
+
+import sun.management.counter.Units;
+
+/**
+ * Copyright (c) 2017 Vadim Voronov
+ * email: vaidim.v.voronov@gmail.com
+ * Created: 20-Feb-17.
+ */
+public class CodeA {
+    public static void app() {
+        System.out.println("\n====CODE_A===");
+        System.out.println("\nJUnit Check\n");
+        Units.
+    }
+}
