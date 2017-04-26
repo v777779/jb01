d59461cc984b2560e032bea38c839e1b549ee477
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-04-26 10:47:48 +0300

    project up ch20 exercise 05

--- src/ch20/ex05/exercise/Exercise.java
+++ src/ch20/ex05/exercise/Exercise.java
@@ -5,7 +5,7 @@ import lib.annot.AtUnitExec;
 /**
  * Vadim Voronov
  * email: vaidim.v.voronov@gmail.com
- * Created: 20-Feb-17.
+ * Created: 26-Apr-17.
  */
 public class Exercise {
     public static void main(String[] args) {
@@ -16,7 +16,7 @@ public class Exercise {
         System.out.println("\n====EXERCISE===");
         System.out.println("\nExercise 05 @Unit Test with Inheritance Check\n");
 
-        AtUnitExec.check("out\\production\\jb01\\ch20\\ex05\\exercise\\HashSetTest");
+        AtUnitExec.check("out\\production\\jb01\\ch20\\ex05\\exercise\\HashSetExternalTest");
 
     }
 }
--- src/hw/Renumber.java
+++ src/hw/Renumber.java
@@ -9,6 +9,6 @@ import lib.files.BFileParser;
  */
 public class Renumber {
     public static void main(String[] args) {
-        BFileParser.check("./src/ch20/ex04/Ex04.java");
+        BFileParser.check("./src/ch20/ex05/Ex05.java");
     }
 }
