25cda6dd3e99940d27a102291b335d73fa2badc3
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-05-07 11:40:27 +0300

    project up ch21 exercise 25

--- src/ch21/ex25/exercise/Exercise.java
+++ src/ch21/ex25/exercise/Exercise.java
@@ -5,7 +5,7 @@ import ch21.ex25.exercise.rest.Restaurant;
 /**
  * Vadim Voronov
  * email: vaidim.v.voronov@gmail.com
- * Created: 20-Feb-17.
+ * Created: 07-May-17.
  */
 public class Exercise {
     public static void app() {
