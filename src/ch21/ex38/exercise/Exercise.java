e30fdc7e82556126b98384d97f4f5399f8667aaa
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-05-14 20:48:45 +0300

    project up ch21 exercise 38 update

--- src/ch21/ex38/exercise/Exercise.java
+++ src/ch21/ex38/exercise/Exercise.java
@@ -5,7 +5,7 @@ import ch21.ex38.exercise.robot.BuildingConstruction;
 /**
  * Vadim Voronov
  * email: vaidim.v.voronov@gmail.com
- * Created: 20-Feb-17.
+ * Created: 12-May-17.
  */
 public class Exercise {
     public static void app() {
