d9044ed38f6ea905db41fdcb8b32bfcb67a49a38
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-05-18 06:16:19 +0300

    project up ch22 exercise 12

--- src/hw/Renumber.java
+++ src/hw/Renumber.java
@@ -9,6 +9,6 @@ import lib.files.BFileParser;
  */
 public class Renumber {
     public static void main(String[] args) {
-        BFileParser.check("./src/ch22/ex11/Ex11.java");
+        BFileParser.check("./src/ch22/ex12/Ex12.java");
     }
 }
