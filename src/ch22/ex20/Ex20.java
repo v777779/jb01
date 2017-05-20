9b575b808cd4f1e8fd71fe973803346592556d0f
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-05-20 20:45:27 +0300

    project up ch22 exercise 20

--- src/ch22/ex20/Ex20.java
+++ src/ch22/ex20/Ex20.java
@@ -1,9 +1,15 @@
 package ch22.ex20;
 
+import ch22.ex20.exercise.Exercise;
+
 /**
  * Vadim Voronov
  * Created: 20-May-17.
  * email: vadim.v.voronov@gmail.com
  */
 public class Ex20 {
+    public static void main(String[] args) {
+        Exercise.app();
+
+    }
 }
--- src/ch22/ex20/exercise/Exercise.java
+++ src/ch22/ex20/exercise/Exercise.java
@@ -3,7 +3,7 @@ package ch22.ex20.exercise;
 /**
  * Vadim Voronov
  * email: vaidim.v.voronov@gmail.com
- * Created: 20-Feb-17.
+ * Created: 20-May-17.
  */
 public class Exercise {
     public static void main(String[] args) {
